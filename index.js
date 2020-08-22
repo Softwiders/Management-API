d5ce4dcf4f167d833af9ed41800ca939f563df7e
Aman <amankarn26@gmail.com>
2020-08-23 00:07:43 +0530

    cors setup

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const sql = require('./Database/connection')
+const morgan = require('morgan')
 
 const ProjectsRouts = require('./Projects/project')
 const IncomeRouts = require('./Income')
@@ -11,11 +11,17 @@ const TransactionRouts = require('./Transaction')
 
 const app = express();
 
+// use logger
+app.use(morgan('tiny'))
+
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
-app.use(bodyParser.json());
+app.use(bodyParser.json())
+
+// for cors authentication of browser
+app.use(cors())
 
 // All API
 app.use("/projects",ProjectsRouts)
@@ -26,7 +32,29 @@ app.use("/transaction",TransactionRouts)
 
 app.use("/", (res, req) => res.send("Welcome"))
 
+
+// set Up status 
+app.use((req,res,next)=>{
+    const error = new Error("Go to Hell");
+    error.status=404;
+    next(error);
+});
+app.use((error,req,res,next)=>{
+   res.status(error.status || 500);
+   res.json({
+       error:{
+           message:error.message
+       }
+   })
+})
+
+app.use((req, res,next)=>{
+    res.status(200).json({
+        message:"Connected"
+    });
+});
+
 //server port
-const port = 3030;
+const port = process.env.port || 3030;
 
 app.listen(port, () => console.log(`Example app listening on port 3030!`));
