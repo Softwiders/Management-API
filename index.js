bd2ac95130da8dfb3dc90d4ad6ef1cdbda68f9d4
Aman <amankarn26@gmail.com>
2020-08-23 08:28:15 +0530

    bug fixed

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan')
+const cors = require('cors')
 
 const ProjectsRouts = require('./Projects/project')
 const IncomeRouts = require('./Income')
